e9297edc879c3086e76ac8f3caa9b716114ff907
Thibault <thibault.baillet@lecnam.net>
2021-03-01 19:23:58 +0100

    modifications Thibault

--- src/question1/PileAdaptor.java
+++ src/question1/PileAdaptor.java
@@ -1,5 +1,23 @@
 package question1;
 
-public class PileAdaptor {
+public class PileAdaptor implements PileI {
+    public StackImpl stack;
+    public PileAdaptor(){
 
+    }
+    public PileAdaptor(StackImpl p_StackImpl){
+        this.stack = p_StackImpl;
+    }
+
+    public void empiler(Object o) {
+        this.stack.push(o);
+    }
+
+    public Object depiler() {
+        return this.stack.pop();
+    }
+
+    public boolean estVide() {
+        return this.stack.isEmpty();
+    }
 }
